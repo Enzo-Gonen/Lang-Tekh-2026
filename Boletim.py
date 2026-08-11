e6243e56cca53659ba5c70e8273bc83e893df1cd
Gonen <tanukikunone@gmail.com>
2026-08-10 21:23:39 -0300

    Create Boletim.py

---
+++ Boletim.py
@@ -0,0 +1,13 @@
+print("Boletim de Lançamento de Notas")
+
+nome = input("Nome do(a) Aluno(a): ")
+curso = input("Curso: ")
+disciplina = input("Disciplina: ")
+nota = int( input("Nota: ")
+
+if nota > 20 and nota < 101:
+           print("Aprovado!")
+
+else nota < 20:
+           print("Reprovado")
+           elif print("Prova Final")
