eace6eb8affc20af19ce5f2446ec63d2fd9646af
Gonen <tanukikunone@gmail.com>
2026-08-17 22:22:37 -0300

    Create entradas.c

---
+++ entradas.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+
+/*
+	int n, res;
+	int bit_64, bit_32, bit_16, bit_8, bit_4, bit_2, bit_1;
+	printf("Insira o valor a ser convertido: ");
+	scanf("%d", &n);
+	
+	bit_64 = n%2;
+	res = n/2;
+	
+	bit_32 = res%2;
+	res = res/2;
+	
+	bit_16 = res%2;
+	res = res/2;
+	
+	bit_8 = res%2;
+	res = res/2;
+	
+	bit_4 = res%2;
+	res = res/2;
+	
+	bit_2 = res%2;
+	res = res/2;
+	
+	printf("O numero %d em bin de %d%d%d%d%d%d%d", n, res%2, bit_2, bit_4, bit_8, bit_16, bit_32, bit_64);
+	
+*/
+	
+	int x1, x2, y1, y2, p1, p2;
+	float dis;
+	
+	printf("Insira o valor do par ordenado na forma (x1,y1): ");
+	scanf("%d %d", &x1, &x2);
+	
+	printf("\n\nLeitura: (%d,%d)", x1, x2);
+	
+	
+	
+	
+	return 0;
+}
